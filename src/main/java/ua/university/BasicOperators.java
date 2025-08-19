d07871582959bfc7299a92c7b8971e7463501bc1
nataliia <nromanen.softserveinc@gmail.com>
2025-08-19 15:08:00 +0100

    add zero lab

---
+++ src/main/java/ua/university/BasicOperators.java
@@ -0,0 +1,150 @@
+package ua.university;
+
+
+/**
+ * BasicOperators class contains a set of static methods
+ * for practicing Java operators, loops, arrays, and branching.
+ */
+public class BasicOperators {
+
+    /**
+     * Returns the sum and average of three integers.
+     *
+     * @param a first integer
+     * @param b second integer
+     * @param c third integer
+     * @return double array where [0] = sum, [1] = average
+     */
+    public static double[] sumAndAverage(int a, int b, int c) {
+        return null;
+    }
+
+    /**
+     * Returns the maximum of three integers.
+     */
+    public static int maxOfThree(int a, int b, int c) {
+        return 0;
+    }
+
+    /**
+     /**
+     * Returns the corresponding grade letter for a given numeric score.
+     * <p>
+     * The grading scale is as follows:
+     * <ul>
+     *     <li>90–100: 'A'</li>
+     *     <li>80–89:  'B'</li>
+     *     <li>70–79:  'C'</li>
+     *     <li>60–69:  'D'</li>
+     *     <li>50–59:  'E'</li>
+     *     <li>0–49:   'F'</li>
+     * </ul>
+     *
+     * @param score the numeric score, expected to be between 0 and 100 inclusive
+     * @return the grade letter corresponding to the given score
+     * @throws IllegalArgumentException if {@code score} is less than 0 or greater than 100
+     */
+    public static char gradeFromScore(int score) {
+        return '0';
+    }
+
+    /**
+     * Returns the day of the week name for a number 1-7.
+     */
+    public static String dayOfWeek(int day) {
+        return null;
+    }
+
+    /**
+     * Returns an array counting down from n to 1.
+     */
+    public static int[] countdown(int n) {
+        return null;
+    }
+
+    /**
+     * Returns factorial of n.
+     */
+    public static long factorial(int n) {
+        return 0;
+    }
+
+    /**
+     * Returns a reversed copy of the array.
+     */
+    public static int[] reverseArray(int[] arr) {
+        return null;
+    }
+
+    /**
+     * Returns sum of all elements in a 2D array (matrix).
+     */
+    public static int sumMatrix(int[][] matrix) {
+        return 0;
+    }
+
+    /**
+     * Checks if a string is a palindrome.
+     */
+    public static boolean isPalindrome(String s) {
+        return false;
+    }
+
+    /**
+     * Returns minimum and maximum of an array.
+     */
+    public static int[] findMinMax(int[] arr) {
+        return null;
+    }
+
+    /**
+     * Returns multiplication table n x n.
+     */
+    public static int[][] multiplicationTable(int n) {
+        return null;
+    }
+
+    /**
+     * Returns all even numbers up to n.
+     */
+    public static int[] evenNumbersUpToN(int n) {
+        return null;
+    }
+
+    /**
+     * Checks if a number is prime.
+     */
+    public static boolean isPrime(int n) {
+        return false;
+    }
+
+    /**
+     * Counts vowels in a string.
+     */
+    public static int countVowels(String s) {
+        return 0;
+    }
+
+    /**
+     * Returns first n Fibonacci numbers.
+     */
+    public static int[] fibonacci(int n) {
+        return null;
+    }
+
+    /**
+     * Returns the transpose of a 2D array (matrix).
+     */
+    public static int[][] transpose(int[][] matrix) {
+        return null;
+    }
+
+    /**
+     * Returns a sorted copy of the array in ascending order.
+     */
+    public static int[] sortArray(int[] arr) {
+        return null;
+    }
+
+}
+
--- src/main/java/ua/university/Main.java
+++ src/main/java/ua/university/Main.java
@@ -1,10 +1,9 @@
 package ua.university;
 
-import ua.university.model.Person;
 
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person("Hjk", "Klkl", "jkj");
-        System.out.println(person);
+
+        System.out.println("Project works");
     }
 }
